27d00dfed2aca71a0695401a5ef80e0cc70d755b
Pedro <pemiguelfernandes1@gmail.com>
2026-04-26 10:49:03 -0300

    Numbers and Words!

---
+++ cpp/showing_numbers_and_words/a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << 123456789 << std::endl;
+    std::cout << "abcdefghi" << std::endl;
+    std::cout << "C++ is cool!" << std::endl;
+    std::cout << "BINARY!" << std::endl;
+    return 0;
+}
