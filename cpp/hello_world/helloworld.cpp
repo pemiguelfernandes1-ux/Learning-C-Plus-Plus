a14f08f755a3675882cfddfab90d80ba01293acc
Pedro <pemiguelfernandes1@gmail.com>
2026-04-26 01:44:39 -0300

    Hello, World

---
+++ cpp/hello_world/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> /*
+Imports the default library "<iostream>" for the compiler. This library enable the compiler
+how to use the keyboard and monitor.
+*/
+
+int main() { // "int main()" is the keyword that the computer looks for to start the program.
+    std::cout << "Hello, World!" << std::endl; // it tells the pc what to display on the screen.
+    return 0; // this tells the computer that the program returned successfully.
+}
