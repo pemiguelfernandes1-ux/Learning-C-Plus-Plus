a34cb88b0bfbfbb14367d7e8b45f3fd15caf2be9
Pedro <pemiguelfernandes1@gmail.com>
2026-04-26 01:45:44 -0300

    Hello, World!

--- cpp/hello_world/helloworld.cpp
+++ cpp/hello_world/helloworld.cpp
@@ -4,6 +4,6 @@ how to use the keyboard and monitor.
 */
 
 int main() { // "int main()" is the keyword that the computer looks for to start the program.
-    std::cout << "Hello, World!" << std::endl; // it tells the pc what to display on the screen.
-    return 0; // this tells the computer that the program returned successfully.
+    std::cout << "Hello, World!" << std::endl; // It tells the pc what to display on the screen.
+    return 0; // This tells the computer that the program returned successfully.
 }
