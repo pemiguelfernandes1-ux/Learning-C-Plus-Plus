67c2ab2b62829e1ea202103dcf8cf998e5d4f9ec
Pedro <pemiguelfernandes1@gmail.com>
2026-04-26 11:48:10 -0300

    Numbers and Words!

---
+++ cpp/showing_numbers_and_words/b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << 1 << std::endl;
+    std::cout << 2 << std::endl;
+    std::cout << 3 << std::endl;
+    std::cout << 4 << std::endl;
+    std::cout << 5 << std::endl;
+    std::cout << 6 << std::endl;
+    std::cout << 7 << std::endl;
+    std::cout << 8 << std::endl;
+    std::cout << 9 << std::endl;
+    return 0;
+}
